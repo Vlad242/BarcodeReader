28e5da390dcfbc88a7353cf4e855c8136929a787
vlad242 <vlad242@i.ua>
2017-11-24 14:04:53 +0200

    Add Barcode Reader Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace BarcodeReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.window = new System.Windows.Forms.PictureBox();
+            this.devices = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.resultBox = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.window)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // window
+            // 
+            this.window.ImageLocation = "";
+            this.window.Location = new System.Drawing.Point(12, 10);
+            this.window.Name = "window";
+            this.window.Size = new System.Drawing.Size(374, 295);
+            this.window.TabIndex = 0;
+            this.window.TabStop = false;
+            // 
+            // devices
+            // 
+            this.devices.FormattingEnabled = true;
+            this.devices.Location = new System.Drawing.Point(58, 20);
+            this.devices.Name = "devices";
+            this.devices.Size = new System.Drawing.Size(156, 21);
+            this.devices.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Devices:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.devices);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(392, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(224, 116);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controll panelle";
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(392, 132);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(224, 173);
+            this.resultBox.TabIndex = 5;
+            this.resultBox.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 317);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.window);
+            this.Name = "Form1";
+            this.Text = "BarcodeReader v 1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.window)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox window;
+        private System.Windows.Forms.ComboBox devices;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox resultBox;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using ZXing;
+
+namespace BarcodeReader
+{
+    public partial class Form1 : Form
+    {
+
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private ZXing.BarcodeReader reader;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        delegate void SetStringDelegate(string parameter);
+
+        void SetResult(string result)
+        {
+            if (!InvokeRequired)
+            {
+                resultBox.Text = result;
+            }
+            else
+            {
+                Invoke(new SetStringDelegate(SetResult), new object[] { result });
+            }
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            reader = new ZXing.BarcodeReader();
+           // reader.Options.PossibleFormats = new List<BarcodeFormat>();
+           // reader.Options.PossibleFormats.Add(BarcodeFormat.QR_CODE);
+
+            if (videoDevices.Count > 0)
+            {
+                foreach(FilterInfo device in videoDevices)
+                {
+                    devices.Items.Add(device.Name);
+                }
+                devices.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            if(videoSource == null)
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[devices.SelectedIndex].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                videoSource.Start();
+            }
+            else
+            {
+                MessageBox.Show("You have runnable scanning proccess!");
+            }
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            window.Image = bitmap;
+            ZXing.Result result = reader.Decode((Bitmap)eventArgs.Frame.Clone());
+            if(result != null)
+            {
+                SetResult(result.Text);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(videoSource != null)
+            {
+                videoSource.Stop();
+            }
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            if (videoSource != null)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                window.Image = null;
+                videoSource = null;
+            }
+            else
+            {
+                MessageBox.Show("Opened reader stream no detected!");
+            }
+        }
+    }
+}
